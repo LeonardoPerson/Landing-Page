f489c7ece7c23822ebca123dea9209e41ec964d5
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-11-07 14:41:30 -0300

    Definição da estrutura principal em grid: .header e .container

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@
 
 const App = () => {
   return (
-    <div className="App">
-      
+    <div className="grid-container">
+      <header className="header">header</header>
+      <div className="container">
+        <section className="section1">section1</section>
+        <section className="section2">section2</section>
+        <footer className="footer">footer</footer>
+      </div>
     </div>
   );
 }
