d09b32a8ed84b057f742acda185867d26db165c9
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-11-07 16:04:34 -0300

    Finalizando a barra de título

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 //import './App.css';
+import barImage from './image/barImage.png'
 
 const App = () => {
   return (
     <div className="grid-container">
-      <header className="header">header</header>
+      <header className="header">
+        <div className="super-bar">
+          <img src={barImage}/>
+          <div className="super-bar-title">SUPERGIANTSGAME</div>  
+        </div>
+      </header>
       <div className="container">
         <section className="section1">section1</section>
         <section className="section2">section2</section>
