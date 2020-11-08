2c2069d098b7445a47bc476e8d0615036e0afc40
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-11-08 19:38:42 -0300

    finalização de todo o header, com todas as imagens responsivas

--- src/header.js
+++ src/header.js
@@ -15,25 +15,25 @@ const Header = () => {
         <div className="super-bar-title">SUPERGIANTSGAME</div>  
       </div>
 
-      <div className="header2">
-        <div className="grupo1-penas">
-          <img src={pena1}/>
-          <img src={pena2}/>
-        </div>
-        <div className="card-header">
-          <div className="text-card-top">TRANSISTOR - RED THE SINGER</div>
-          <img className="card-mulher" src={mulher} alt="desenho de mulher"/>
-          <div className="text-card-bottom">
-            "Olha, o que quer que você <br/>
-             esteja pensando, me faça <br/> 
-             um favor, não solte."
+      <div className="header2">       
+          <div className="card-header">
+            <div className="grupo1-penas">
+              <img className="pena1" src={pena1}/>
+              <img className="pena2" src={pena2}/>
+            </div>
+            <div className="text-card-top">TRANSISTOR - RED THE SINGER</div>
+            <img className="card-mulher" src={mulher} alt="desenho de mulher"/>
+            <div className="text-card-bottom">
+              "Olha, o que quer que você <br/>
+              esteja pensando, me faça <br/> 
+              um favor, não solte."
+            </div>
+            <div className="grupo2-penas">
+              <img className="pena3" src={pena3}/>
+              <img className="pena4" src={pena4}/>
+            </div>
           </div>
-        </div>
-        <div className="grupo2-penas">
-          <img src={pena3}/>
-          <img src={pena4}/>
-        </div>
-      </div>
+        </div>      
     </header>
   )
 }
