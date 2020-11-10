31ff41e0397bbae711d48c165ea691c00ed67f96
Leonardo Muros do Nascimento <lmuros@gmail.com>
2020-11-10 01:06:35 -0300

    inserindo o botão para âncora no footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,7 +2,11 @@ import './footer.css'
 
 const Footer = () => {
   return(
-    <footer className="footer"></footer>
+    <footer className="footer">
+      <div className="return-top">
+        <a className="arrow-footer" href='#header'></a>
+      </div>
+    </footer>
   )
 }
 
--- src/header/Header.js
+++ src/header/Header.js
@@ -9,34 +9,34 @@ import './header.css'
 
 const Header = () => {
   return(
-    <header className="header">
-
+    <header id="header">
       <div className="super-bar">
         <img src={barImage} alt="icone supergiantesgame"/>
         <div className="super-bar-title">SUPERGIANTSGAME</div>  
       </div>
 
       <div className="header2">       
-          <div className="card-header">
-            <div className="grupo1-penas">
-              <img className="pena1" src={pena1}/>
-              <img className="pena2" src={pena2}/>
-            </div>
-            <div className="text-card-top">TRANSISTOR - RED THE SINGER</div>
-            <img className="card-mulher" src={mulher} alt="desenho de mulher"/>
-            <div className="text-card-bottom">
-              "Olha, o que quer que você <br/>
-              esteja pensando, me faça <br/> 
-              um favor, não solte."
-            </div>
-            <div className="grupo2-penas">
-              <img className="pena3" src={pena3}/>
-              <img className="pena4" src={pena4}/>
-            </div>
+        <div className="card-header">
+          <div className="grupo1-penas">
+            <img className="pena1" src={pena1}/>
+            <img className="pena2" src={pena2}/>
           </div>
-        </div>      
+
+          <div className="text-card-top">TRANSISTOR - RED THE SINGER</div>
+          <img className="card-mulher" src={mulher} alt="desenho de mulher"/>
+          <div className="text-card-bottom">
+            "Olha, o que quer que você <br/>
+            esteja pensando, me faça <br/> 
+            um favor, não solte."
+          </div>
+
+          <div className="grupo2-penas">
+            <img className="pena3" src={pena3}/>
+            <img className="pena4" src={pena4}/>
+          </div>
+        </div>
+      </div>      
     </header>
   )
 }
-
 export default Header
